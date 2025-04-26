a738fea42621668e6226029ceea78e9023b3196f
progwi0 <93661506+progwi0@users.noreply.github.com>
2025-04-26 10:48:55 +0300

    Add files via upload

--- fifi.py
+++ fifi.py
@@ -1,13 +1,24 @@
 import os
-from tkinter import *
+import webbrowser
 from tkinter import messagebox as dialogus
 from tkinter import filedialog
+from ttkbootstrap import *
+from ttkbootstrap.constants import *
 
-app = Tk()
+app = Window(themename="litera")
 app.title("Fifi")
 
+homus = os.path.expanduser("~")
+patus = os.path.join(homus, ".progwi0")
+themepath = os.path.join(patus, "theme.txt")
+
+simplus = "~/.progwi0/theme.txt"
+
+if not os.path.exists(simplus):
+    os.system("cd ~ && mkdir .progwi0 && cd .progwi0 && touch theme.txt")
+
 def info():
-    dialogus.showinfo("🪶", "Fifi 1.0\nCreated in 2025 by progwi0.")
+    dialogus.showinfo("🪶", "Fifi 5.0\nCreated in 2025 by progwi0.")
 
 def save():
     filename = filedialog.asksaveasfilename(defaultextension=".txt",
@@ -24,22 +35,42 @@ def openf():
         text.delete(1.0, "end")
         text.insert("1.0", file.read())
 
+def light():
+    app.style.theme_use("litera")
+    with open(themepath, "w") as file:
+        file.write("litera")
+
+def dark():
+    app.style.theme_use("darkly")
+    with open(themepath, "w") as file:
+        file.write("darkly")
+
 def menus():
     menu.post(app.winfo_pointerx(), app.winfo_pointery())
 
-feather = Button(app, text = "🪶", command = lambda:menu.post(app.winfo_pointerx(), app.winfo_pointery()))
+feather = Button(app, text = "🪶", command = lambda:menu.post(app.winfo_pointerx(), app.winfo_pointery()), bootstyle = SECONDARY)
 feather.pack(fill = "x")
 
 text = Text(app)
 text.pack(fill = "both", expand = True)
 
 menu = Menu(app, tearoff = 0)
+menu.add_separator()
 menu.add_command(label="New", command = lambda:os.system("fifi"))
 menu.add_command(label="Open", command = openf)
 menu.add_command(label="Save", command = save)
 menu.add_separator()
-menu.add_command(label="Reinstall (Only for pix version)", command = lambda:os.system("pix reinstall fifi"))
+menu.add_command(label="Light theme", command = light)
+menu.add_command(label="Dark theme", command = dark)
 menu.add_separator()
+menu.add_command(label="Update (Only for pix version)", command = lambda:os.system("pix reinstall fifi"))
+menu.add_separator()
+menu.add_command(label="My site", command = lambda:webbrowser.open("https://progwi0.github.io/"))
 menu.add_command(label="About", command = info)
+menu.add_separator()
+
+with open(themepath, "r") as file:
+        themus = file.read().strip()
+        app.style.theme_use(themus)
 
 app.mainloop()
